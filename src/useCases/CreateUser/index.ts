5c2e0cba5f2189ed56f5d14377c379c35a6eacfe
Bruno Brancalhao <bruno@irroba.com.br>
2020-10-20 20:03:03 -0300

    initial changes

---
+++ src/useCases/CreateUser/index.ts
@@ -0,0 +1,19 @@
+import { MailtrapMailProvider } from "../../providers/implementations/MailtrapMailProvider"
+import { PostgresUsersRepository } from "../../repositories/implementations/PostgresUsersRepository"
+import { CreateUserController } from "./CreateUserController";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+const mailtrapMailProvider = new MailtrapMailProvider()
+
+const postgresUserRepository = new PostgresUsersRepository();
+
+const createUserUseCase = new CreateUserUseCase(
+    postgresUserRepository,
+    mailtrapMailProvider
+);
+
+const createUserController = new CreateUserController(
+    createUserUseCase
+);
+
+export { createUserUseCase, createUserController }
